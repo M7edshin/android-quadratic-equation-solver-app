451a87cdb06cef377bc0a0d1c034a3e789c1c538
M7edshin <M7edshin@hotmail.com>
2017-07-18 18:48:29 +0100

    Menu options added

--- app/src/main/java/shahin/simplequadraticsolver/MainActivity.java
+++ app/src/main/java/shahin/simplequadraticsolver/MainActivity.java
@@ -1,8 +1,12 @@
 package shahin.simplequadraticsolver;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +16,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     //Views Declaration
-    EditText txtA,txtB,txtC;
-    TextView tvSol1,tvSol2,vReason,tvInfo;
+    EditText txtA, txtB, txtC;
+    TextView tvSol1, tvSol2, vReason, tvInfo;
     MediaPlayer mPlayer;
     Button btnRound, btnSolution;
 
@@ -34,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         txtA = (EditText) findViewById(R.id.txtA);
         txtB = (EditText) findViewById(R.id.txtB);
         txtC = (EditText) findViewById(R.id.txtC);
-        btnRound = (Button)findViewById(R.id.btnRound) ;
+        btnRound = (Button) findViewById(R.id.btnRound);
 
         tvSol1.setVisibility(View.INVISIBLE);
         tvSol2.setVisibility(View.INVISIBLE);
@@ -43,11 +47,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_options items for use in the action bar
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btnMyApps:
+                String urlWebsite = "https://play.google.com/store/apps/developer?id=Mohamed+Shahin";
+                Uri uriWebpage = Uri.parse(urlWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriWebpage);
+                if (intentWebsite.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intentWebsite);
+                }
+                return true;
+            case R.id.btnReport:
+                sendEmail("Report a bug");
+                return true;
+            case R.id.btnSuggest:
+                sendEmail("Suggest a feature or recommendation");
+                return true;
+            case R.id.btnExit:
+                finish();
+                System.exit(0);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     //Getting it in separate method
-    private double discriminant(double a, double b, double c){
+    private double discriminant(double a, double b, double c) {
         double disc = 0;
         //b2 – 4ac
-        disc = Math.pow(b,2) - (4*a*c);
+        disc = Math.pow(b, 2) - (4 * a * c);
         return disc;
     }
 
@@ -58,18 +95,18 @@ public class MainActivity extends AppCompatActivity {
 
         //Nested If statement also if statement within if statement to control the entries (Error Trap)
         if (!txtA.getText().toString().isEmpty() && !txtB.getText().toString().isEmpty() && !txtC.getText().toString().isEmpty() &&
-            !txtA.getText().toString().equals(".") && !txtB.getText().toString().equals(".") && !txtC.getText().toString().equals(".") &&
-            !txtA.getText().toString().equals("-") && !txtB.getText().toString().equals("-") && !txtC.getText().toString().equals("-") ) {
+                !txtA.getText().toString().equals(".") && !txtB.getText().toString().equals(".") && !txtC.getText().toString().equals(".") &&
+                !txtA.getText().toString().equals("-") && !txtB.getText().toString().equals("-") && !txtC.getText().toString().equals("-")) {
 
             double a = Double.parseDouble(txtA.getText().toString());
             double b = Double.parseDouble(txtB.getText().toString());
             double c = Double.parseDouble(txtC.getText().toString());
 
-            if(a == 0){
-                mPlayer = MediaPlayer.create(this,R.raw.error);
+            if (a == 0) {
+                mPlayer = MediaPlayer.create(this, R.raw.error);
                 mPlayer.start();
-                Toast.makeText(getApplicationContext(), R.string.str_coefficient,Toast.LENGTH_SHORT).show();
-            }else {
+                Toast.makeText(getApplicationContext(), R.string.str_coefficient, Toast.LENGTH_SHORT).show();
+            } else {
                 if (discriminant(a, b, c) > 0) {
                     sol1 = (-b + Math.sqrt(discriminant(a, b, c))) / (2 * a);
                     sol2 = (-b - Math.sqrt(discriminant(a, b, c))) / (2 * a);
@@ -81,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
                     tvSol2.setVisibility(View.VISIBLE);
                     vReason.setVisibility(View.VISIBLE);
                     btnRound.animate().alpha(1).setDuration(2000);
-                    mPlayer = MediaPlayer.create(this,R.raw.clicks137);
+                    mPlayer = MediaPlayer.create(this, R.raw.clicks137);
                     mPlayer.start();
                 } else if (discriminant(a, b, c) == 0) {
                     sol1 = (-b + Math.sqrt(discriminant(a, b, c))) / (2 * a);
@@ -91,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
                     tvSol1.setVisibility(View.VISIBLE);
                     vReason.setVisibility(View.VISIBLE);
                     btnRound.animate().alpha(1).setDuration(2000);
-                    mPlayer = MediaPlayer.create(this,R.raw.clicks137);
+                    mPlayer = MediaPlayer.create(this, R.raw.clicks137);
                     mPlayer.start();
                 } else {
                     reason = getString(R.string.str_no_solution);
@@ -103,29 +140,41 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-        }else{
+        } else {
 
-            mPlayer = MediaPlayer.create(this,R.raw.error);
+            mPlayer = MediaPlayer.create(this, R.raw.error);
             mPlayer.start();
             tvSol1.setVisibility(View.INVISIBLE);
             tvSol2.setVisibility(View.INVISIBLE);
             vReason.setVisibility(View.INVISIBLE);
             btnRound.animate().alpha(0).setDuration(0);
-            Toast.makeText(getApplicationContext(), R.string.str_enter_cofficient,Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), R.string.str_enter_cofficient, Toast.LENGTH_SHORT).show();
 
         }
     }
 
 
-    public void roundNo(View view){
+    public void roundNo(View view) {
 
         String roundSol1 = String.format("%.2f", sol1);
         String roundSol2 = String.format("%.2f", sol2);
 
         tvSol1.setText(getString(R.string.str_positive_equal) + roundSol1);
         tvSol2.setText(getString(R.string.str_negative_equal) + roundSol2);
-        mPlayer = MediaPlayer.create(this,R.raw.clicks148);
+        mPlayer = MediaPlayer.create(this, R.raw.clicks148);
         mPlayer.start();
     }
 
+    private void sendEmail(String text) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"M7edshin@gmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, text);
+        i.putExtra(Intent.EXTRA_TEXT, "Your Message");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There are no email clients or apps installed on your device.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
