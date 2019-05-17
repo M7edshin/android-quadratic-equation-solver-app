ea44f586d92bdf0f4f685ce702b5d4e63addb711
Shah91n <shahin@outlook.ie>
2019-05-17 14:46:50 +0100

    fixed untracked files

--- app/src/main/java/shahin/simplequadraticsolver/MainActivity.java
+++ app/src/main/java/shahin/simplequadraticsolver/MainActivity.java
@@ -4,36 +4,46 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.material.snackbar.Snackbar;
 import com.romainpiel.shimmer.Shimmer;
 import com.romainpiel.shimmer.ShimmerTextView;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.et_a) EditText et_a;
-    @BindView(R.id.et_b) EditText et_b;
-    @BindView(R.id.et_c) EditText et_c;
-    @BindView(R.id.tv_sol_one) TextView tv_sol_one;
-    @BindView(R.id.tv_sol_two) TextView tv_sol_two;
-    @BindView(R.id.tv_reason) TextView tv_reason;
-    @BindView(R.id.btn_round) TextView btn_round;
-    @BindView(R.id.adView) AdView adView;
-    @BindView(R.id.tv_info) ShimmerTextView tv_info;
+    @BindView(R.id.et_a)
+    EditText et_a;
+    @BindView(R.id.et_b)
+    EditText et_b;
+    @BindView(R.id.et_c)
+    EditText et_c;
+    @BindView(R.id.tv_sol_one)
+    TextView tv_sol_one;
+    @BindView(R.id.tv_sol_two)
+    TextView tv_sol_two;
+    @BindView(R.id.tv_reason)
+    TextView tv_reason;
+    @BindView(R.id.btn_round)
+    TextView btn_round;
+    @BindView(R.id.adView)
+    AdView adView;
+    @BindView(R.id.tv_info)
+    ShimmerTextView tv_info;
 
     private MediaPlayer mPlayer;
     private double sol1, sol2;
@@ -45,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        MobileAds.initialize(this, "Your Ad ID");
+        MobileAds.initialize(this, getString(R.string.ADMOB_APP_ID));
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
 
@@ -59,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu_options items for use in the action bar
         getMenuInflater().inflate(R.menu.menu_options, menu);
         return true;
     }
@@ -67,10 +76,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-
-            case R.id.action_report_suggest:
-                sendEmail(getString(R.string.action_suggestion));
-                return true;
 
             case R.id.action_equation_info:
                 String url = "https://en.wikipedia.org/wiki/Quadratic_equation";
@@ -84,6 +89,10 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_rate:
                 startActivity(new Intent(Intent.ACTION_VIEW,
                         Uri.parse("market://details?id=shahin.simplequadraticsolver")));
+                return true;
+
+            case R.id.action_policy:
+                openPrivacyPolicy();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -155,16 +164,13 @@ public class MainActivity extends AppCompatActivity {
             tv_sol_two.setVisibility(View.INVISIBLE);
             tv_reason.setVisibility(View.INVISIBLE);
             snackBarShort(getString(R.string.str_enter_cofficient));
-
         }
     }
 
 
     public void roundNo(View view) {
-
-        String roundSol1 = String.format("%.2f", sol1);
-        String roundSol2 = String.format("%.2f", sol2);
-
+        String roundSol1 = String.format(Locale.getDefault(), "%.2f", sol1);
+        String roundSol2 = String.format(Locale.getDefault(), "%.2f", sol2);
         tv_sol_one.setText(String.format("%s %s", getString(R.string.str_positive_equal), roundSol1));
         tv_sol_two.setText(String.format("%s %s", getString(R.string.str_negative_equal), roundSol2));
         mPlayer = MediaPlayer.create(this, R.raw.clicks148);
@@ -181,24 +187,16 @@ public class MainActivity extends AppCompatActivity {
         et_a.requestFocus();
     }
 
-    private void snackBarShort(String message){
+    private void snackBarShort(String message) {
         Snackbar snackbar = Snackbar
                 .make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG);
         snackbar.show();
     }
 
-    private void sendEmail(String text) {
-        Intent i = new Intent(Intent.ACTION_SEND);
-        i.setType("message/rfc822");
-        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"M7edshin@gmail.com"});
-        i.putExtra(Intent.EXTRA_SUBJECT, text);
-        i.putExtra(Intent.EXTRA_TEXT, "Your Message");
-        try {
-            startActivity(Intent.createChooser(i, "Send mail..."));
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this, "There are no email clients or apps installed on your device.", Toast.LENGTH_SHORT).show();
-        }
+    private void openPrivacyPolicy() {
+        String url = "https://github.com/M7edshin/Privacy-Policy";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
-
-
 }
